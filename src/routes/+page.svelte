de1eb5ff073e4956df4320a45d67146d61fffef7
isabellhaid <isabellhaid04@gmail.com>
2024-03-08 08:29:12 +0100

    verbesserungen

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,20 +3,19 @@
 	let promise = supabase.from('countries').select();
 </script>
 
-<h1>Aufgaben:</h1>
-<ul>
-	<li>
-		anstatt eines JS-Objekts, das auf der Seite dargestellt wird, möchte ich eine Liste aller Länder
-	</li>
-	<li>loading symbol</li>
-	<li>Vercel</li>
-</ul>
-{#await promise}
-	<div><span class="loading loading-spinner loading-lg" /></div>
-{:then result}
-	<ul>
-		{#each result.data as country}
-			<li>{country.name}</li>
-		{/each}
-	</ul>
-{/await}
+<div class="m-8">
+	<h1 class="text-8xl my-3 text-center">Home</h1>
+	<p class="text-2xl my-3 text-center">
+		Exolore our world. Discover new places. Welcome to our home.
+	</p>
+	<h1 class="text-4xl my-2 px-3">Countries:</h1>
+	{#await promise}
+		<div><span class="loading loading-spinner loading-lg" /></div>
+	{:then result}
+		<ul class="list-disc px-6 text-1.5xl">
+			{#each result.data as country}
+				<li>{country.name}</li>
+			{/each}
+		</ul>
+	{/await}
+</div>
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+    import Auth from '$lib/Auth.svelte';
+</script>
+
+<h1 class="text-8xl my-3 text-center">Login</h1>
+<h1 class="header text-2xl text-center">Supabase + Svelte</h1>
+		<p class="description text-xl my-3 px-8">Sign in via magic link with your email below</p>
+<Auth />

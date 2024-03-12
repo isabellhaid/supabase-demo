4f6e7901fbc22d384f11d835522f465e805d1758
isabellhaid <isabellhaid04@gmail.com>
2024-03-12 14:39:32 +0100

    video

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -3,6 +3,7 @@
 	import type { AuthSession } from '@supabase/supabase-js';
 	import { supabase } from '$lib/supabase';
 	import { session } from '$lib/store';
+	import Avatar from '$lib/Avatar.svelte';
 
 	let loading = false;
 	let username: string | null = null;
@@ -73,7 +74,9 @@
 	<p class="text-2xl my-3 text-center">View your profile!</p>
 </div>
 {#if $session}
+	
 	<form on:submit|preventDefault={updateProfile} class="form-widget my-2 px-10">
+		<Avatar size={150} bind:url={avatarUrl} on:upload={updateProfile}/>
 		<div class="text-xl my-2">Email: {$session.user.email}</div>
 		<div class="text-xl my-2">
 			<label for="username">Name</label>

c500f1a00f529c4b8886d779e114cc96dbf8a755
isabellhaid <isabellhaid04@gmail.com>
2024-03-19 14:56:08 +0100

    video

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,51 @@
 <script>
 	import { supabase } from '$lib/supabase';
 	let promise = supabase.from('countries').select();
+	let names = supabase.from('rabbits').select();
+	let name = '';
+
+	async function sendName() {
+		const response = await fetch('/api/sendName', {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({ name }),
+		})
+	;
+	const data = await response.text();
+	console.log(data);
+	name ="";
+	names = supabase.from('rabbits').select();
+	}
 </script>
 
-<div class="m-8">
+<div class="prose">
+	<h1>APIs</h1>
+	<ul>
+		<li><a href="/api/hello?name=Isabell">Link zur API mit Query-Parameter</a></li>
+		<li><a href="/api/hello/Isabell">Link zur API mit Route-Parameter</a></li>
+	</ul>
+
+	<form>
+		<h2>All my rabbits</h2>
+		<input type="text" bind:value={name} /> <br /><button class="btn" on:click={sendName}>Add rabbit!</button>
+	</form>
+
+	<h2>Rabbit names</h2>
+
+	{#await names}
+    <span class="loading loading-bars loading-lg text-primary" />
+{:then result}
+    <ul>
+        {#each result.data as rabbit (rabbit.id)}
+            <li>{rabbit.name}</li>
+        {/each}
+    </ul>
+{/await}
+</div>
+
+<!-- <div class="m-8">
 	<h1 class="text-8xl my-3 text-center">Home</h1>
 	<p class="text-2xl my-3 text-center">
 		Exolore our world. Discover new places. Welcome to our home.
@@ -18,4 +60,4 @@
 			{/each}
 		</ul>
 	{/await}
-</div>
+</div> -->
